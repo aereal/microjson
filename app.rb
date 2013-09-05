793cc0ee76a7c98dcf519702c4e14d931011c60c
aereal <aereal@aereal.org>
2013-09-06 01:21:22 +0900

    Support Treasure Data logging

--- app.rb
+++ app.rb
@@ -12,17 +12,15 @@ REDIS =
     Redis.new
   end
 
-enable :logging
+set :td_table, "microjson.development"
 
 before '/' do
-  @checkpoints = [Time.now]
   content_type :json
 end
 
 helpers do
-  def check(stack)
-    stack.push(Time.now)
-    stack.reverse.take(2).reduce(:-)
+  def td(object = {})
+    $stdout.puts("@%s %s" % [settings.td_table, object.merge(time: Time.now.to_i).to_json])
   end
 end
 
@@ -30,21 +28,21 @@ get '/' do
   app_id = params[:app_id]
   halt 400, { message: 'Invalid app_id' }.to_json unless app_id && %r{^[^/]+$} === app_id
   if value = REDIS.get(app_id)
-    logger.info "Cache Hit: #{app_id} (#{ check(@checkpoints) })"
+    td app_id: app_id, event: :cache_hit
     value
   else
-    logger.info "Cache Miss: #{app_id}"
+    td app_id: app_id, event: :cache_miss
     url = URI.parse("https://chrome.google.com/webstore/detail/#{ Rack::Utils.escape(app_id) }/details")
     begin
       content = url.read
-      logger.info "Get #{url} (#{check(@checkpoints)})"
+      td app_id: app_id, event: :get_url
       doc = Mida::Document.new(content, url)
-      logger.info "Parse #{url} (#{check(@checkpoints)})"
+      td app_id: app_id, event: :parse_html
       doc.items.map(&:to_h).to_json.tap do |structure|
         REDIS.set(app_id, structure)
-        logger.info "Store #{app_id} (#{ check(@checkpoints) })"
       end
     rescue OpenURI::HTTPError => e
+      td app_id: app_id, event: :fail_get_url
       halt 400, { message: "Fail to access to Chrom Web Store" }.to_json
     end
   end
